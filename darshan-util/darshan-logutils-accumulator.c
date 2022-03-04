f7b16de8992eb78fcea7042f6a7ce8ea4fc6e547
Phil Carns <carns@mcs.anl.gov>
2022-03-04 15:33:15 -0500

    incremental progress on accumulator skeletons

--- darshan-util/darshan-logutils-accumulator.c
+++ darshan-util/darshan-logutils-accumulator.c
@@ -8,16 +8,26 @@
  * functions in darshan-logutils.h.
  */
 
+#include <stdlib.h>
+
 #include "darshan-logutils.h"
 
 struct darshan_accumulator_st {
-
+    darshan_module_id module_id;
+    void* total_record;
 };
 
 int darshan_accumulator_create(darshan_module_id id,
                                darshan_accumulator*   new_accumulator)
 {
-    return(-1);
+    *new_accumulator = calloc(1, sizeof(struct darshan_accumulator_st));
+    if(!(*new_accumulator))
+        return(-1);
+
+    (*new_accumulator)->module_id = id;
+
+    /* TODO: allocate total_record, according to module type */
+    return(0);
 }
 
 int darshan_accumulator_inject(darshan_accumulator accumulator,
@@ -36,5 +46,9 @@ int darshan_accumulator_emit(darshan_accumulator             accumulator,
 
 int darshan_accumulator_destroy(darshan_accumulator accumulator)
 {
-    return(-1);
+    if(accumulator->total_record)
+        free(accumulator->total_record);
+    free(accumulator);
+
+    return(0);
 }
