8a158f8ecfba9c6ea58d4935d89b75e4dd8aaffd
Phil Carns <carns@mcs.anl.gov>
2022-03-09 15:56:42 -0500

    add unit testing infrastructure
    - just stubs for now, but the idea is to have a way to exercise the
      accumulator routines while they are under development before
      incorporating into darshan-parser

---
+++ darshan-util/tests/unit-tests/darshan-accumulator.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include "munit/munit.h"
+
+struct test_context {
+};
+
+static void* test_context_setup(const MunitParameter params[], void* user_data)
+{
+    (void) params;
+    (void) user_data;
+    struct test_context* ctx = calloc(1, sizeof(*ctx));
+
+    return ctx;
+}
+
+static void test_context_tear_down(void *data)
+{
+    struct test_context *ctx = (struct test_context*)data;
+
+    free(ctx);
+}
+
+/* test repeated init/finalize cycles, server mode */
+static MunitResult stub(const MunitParameter params[], void* data)
+{
+    struct test_context* ctx = (struct test_context*)data;
+
+    printf("Hello world.");
+
+    return MUNIT_OK;
+}
+
+static MunitTest tests[] = {
+    { "/stub", stub, test_context_setup, test_context_tear_down, MUNIT_TEST_OPTION_NONE, NULL},
+    { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+static const MunitSuite test_suite = {
+    "/darshan-accumulator", tests, NULL, 1, MUNIT_SUITE_OPTION_NONE
+};
+
+
+int main(int argc, char **argv)
+{
+    return munit_suite_main(&test_suite, NULL, argc, argv);
+}
